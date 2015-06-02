e2cdc1c1f309469e4dc97339f5afe445acb84d12
Alexandr Kovtunov <1sasha.sasha1@gmail.com>
2015-06-02 12:49:48 +0300

    Cleaned stuff

--- baio.js
+++ baio.js
@@ -6,15 +6,14 @@
 
 			name: 'BAIO',
 
+      response_type: 'token',
+
 			oauth: {
 				version: "2.0",
 				auth: base + 'oauth/authorize',
 				grant: 'authorization_code'
 			},
 
-			// scope: {
-			// 	basic: 'users/me'
-			// },
 			base: base + 'api/v2/',
 
 			get: {
@@ -24,25 +23,10 @@
 
 			wrap: {
 				me: function(o) {
-					if (o.id) {
-						o.last_name = o.last_name || (o.name ? o.name.last_name : null);
-						o.first_name = o.first_name || (o.name ? o.name.first_name : null);
-
-						if (o.email) {
-							o.email = o.email;
-						}
-					}
-
 					return o;
 				}
 			},
 
-			
-
-			// Don't even try submitting via form.
-			// This means no POST operations in <=IE9
-
-			//jsonp: false
 		}
 	})
 })(hello);
